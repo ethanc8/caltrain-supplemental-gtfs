bdd61ae32027c24bc24a5354c2813bfb8f4c7070
ethanc8 <echaroenpitaks@gmail.com>
2025-12-28 23:55:30 -0600

    Prep for run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 python3 timetable-to-gtfs.py \
-  "../input-data/Lagos timetables.xlsx" \
-  --sheets "BLUE-Outbound,BLUE-Inbound,RED-Inbound,RED-Outbound" \
+  "../input-data/NYE timetables.xlsx" \
+  --sheets "Caltrain" \
   --outdir ../gtfs
